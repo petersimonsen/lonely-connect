f97b002eeb645189ead940ebf41f4a54e896084e
Peter Simonsen <peter@Peters-MacBook-Pro.local>
2024-07-30 23:02:52 -0600

    adding nyt

---
+++ server/nyt.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+const getPuzzle = async () => {
+	const date = new Date();
+	const day = `${date.getDate()}`.length === 1 ? `0${date.getDate()}` : date.getDate();
+	const month = `${date.getMonth()}`.length === 1 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+	const url = `https://www.nytimes.com/svc/connections/v2/${date.getFullYear()}-${month}-${day}.json`;
+	const daily = await axios.get(url);
+	return daily;
+}
+
+module.exports = { getPuzzle };
