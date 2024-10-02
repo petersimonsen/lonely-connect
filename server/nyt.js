038c802f1a33f412d923e2e5b1f35b161df1a6d1
Peter Simonsen <peter@Peters-MBP.fios-router.home>
2024-10-02 00:21:40 -0400

    month setup

--- server/nyt.js
+++ server/nyt.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 const getPuzzle = async () => {
 	const date = new Date();
-	const day = `${date.getDate()}`.length === 1 ? `0${date.getDate()}` : `${date.getDate()}`;
-	const month = `${date.getMonth()}`.length === 1 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+	const day = `${date.getDate()}`.length === 1 ? `0${date.getDate()}` : date.getDate();
+	const month = `${date.getMonth() + 1}`.length === 1 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
 	const url = `https://www.nytimes.com/svc/connections/v2/${date.getFullYear()}-${month}-${day}.json`;
 	const daily = await axios.get(url);
 	return daily;
